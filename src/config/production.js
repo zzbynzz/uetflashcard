7113fc6127bd962abb284eb008da9bab6411a47d
Tran Viet Thang <uendno@gmail.com>
2017-02-27 09:59:02 +0700

    sdasd

---
+++ src/config/production.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mongodb: {
+        url: 'mongodb://root:root@ds161209.mlab.com:61209/flashcard',
+        options: {
+            clustered: false
+        }
+    },
+    server: {
+        name: 'Flashcard-Development-Server',
+        protocol: 'http',
+        host: 'localhost',
+        port: 9100,
+        requestExpiry: {header: 'x-request-expiry-time'},
+        throttle: {
+            burst: 100,
+            rate: 50,
+            ip: true,
+            overrides: {
+                '192.168.1.1': {
+                    rate: 0,        // unlimited
+                    burst: 0
+                }
+            }
+        }
+    }
+}
